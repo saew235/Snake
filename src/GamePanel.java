31088f2f0157ff8316255b570090e6e8c08fa434
WArerAQ <bebichandcyra@gmail.com>
2020-12-24 16:35:13 +0500

    fdfdfd

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -6,7 +6,6 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.Random;
 
-// коммит прошёл :)
 
 public class GamePanel extends JPanel implements ActionListener {
 
