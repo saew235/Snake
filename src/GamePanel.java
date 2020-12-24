7f355ee0057b74c711ab77da1d8d6ce11467e432
WArerAQ <bebichandcyra@gmail.com>
2020-12-24 16:45:58 +0500

    213332

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -4,9 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.util.Random;
+import java.util.Random;d
 
-
+dsfsdfs
 public class GamePanel extends JPanel implements ActionListener {
 
     static final int SCREEN_WIDTH = 600;
